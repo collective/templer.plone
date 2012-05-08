9ac0f9f1c35d3416a8e545f22bc9f863c4427d53
cewing <cris@crisewing.com>
2012-05-07 22:23:29 -0700

    * alter printed description and help text based on the presence/absence of local command support

--- src/templer/plone/archetype.py
+++ src/templer/plone/archetype.py
@@ -9,20 +9,31 @@ try:
 except ImportError:
     SUPPORTS_LOCAL_COMMANDS = False
 
+HELP_TEXT = """
+This creates a Plone add-on package with the required infrastructure
+to create custom Archetypes content types.
+"""
+
+POST_RUN_TEXT = """"""
+
+if SUPPORTS_LOCAL_COMMANDS:
+    HELP_TEXT += """
+This template supports local commands.  These commands allow you to
+generate skeleton content types and add fields and other features to
+your new types.
+"""
+    POST_RUN_TEXT = """
+Your new package supports local commands.  To access them, cd into
+the 'src' directory inside your new package.  From there, you can run
+'paster add --list` to see the list of local commands.
+"""
+
+
 class Archetype(Plone):
     _template_dir = 'templates/archetype'
     summary = 'A Plone project that uses Archetypes content types'
-    help = """
-This creates a Plone project that uses Archetypes content types.
-It has local commands that will allow you to add content types
-and to add fields to your new content types.
-"""
-    post_run_msg = """
-There is a local command to add individual Archetype content types
-and to add fields to those content types. See the instructions above
-on how to use this command.
-"""
-
+    help = HELP_TEXT
+    post_run_msg = POST_RUN_TEXT
     required_templates = ['plone_basic']
     use_cheetah = True
     use_local_commands = SUPPORTS_LOCAL_COMMANDS
@@ -35,7 +46,7 @@ on how to use this command.
         modes=(EASY, EXPERT),
         default='Example Name',
         help="""
-This becomes the title of the project. It is used in the 
+This becomes the title of the project. It is used in the
 GenericSetup registration for the project and, as such, appears
 in Plone's Add/Remove products form.
 """
