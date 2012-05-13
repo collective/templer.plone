3200d37945e95519839a03d24c1c81f6b8e44e3d
cewing <cris@crisewing.com>
2012-05-12 22:29:56 -0700

    * add a new structure to insert a GS profile in a nested namespace package

--- src/templer/plone/structures.py
+++ src/templer/plone/structures.py
@@ -3,3 +3,7 @@ from templer.core.structures import Structure
 
 class ProfileStructure(Structure):
     _structure_dir = 'structures/namespace_profile'
+
+
+class NestedProfileStructure(Structure):
+    _structure_dir = 'structures/nested_namespace_profile'
